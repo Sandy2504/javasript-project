01cdfa260cc9c8a07b25b763fc60697bfd2923b9
Sandra Davies <dav.sandra1970@gmail.com>
2024-03-18 20:03:51 +0100

    string-operator-datatypes

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+console.log(`Sandra Davies`);
+console.log("S" + "a" + "n" + "d" + "r" + "a");
+console.log("Hallo");
+console.log("That doesn`t work");
+console.log("333" === 333);
+console.log((600 + 510) / 2);
+console.log(2 + 2);
+
+console.log(25 / 5) === 50 / 10;
